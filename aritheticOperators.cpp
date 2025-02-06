3cf6ad69555b60bcd4434d2dfebbfea7ac97b2b7
pallavi23102003 <pallavishirsath2024@gmail.com>
2025-02-06 12:25:26 +0530

    new

--- aritheticOperators.cpp
+++ aritheticOperators.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int main(){
 
+    // int x = 7, y = 2;
+    // cout<<x+y<<endl;
+    // cout<<x-y<<endl;
+    // cout<<x*y<<endl;
+    // cout<<x/y<<endl;
+    
     float x = 7, y = 2;
     cout<<x+y<<endl;
     cout<<x-y<<endl;
