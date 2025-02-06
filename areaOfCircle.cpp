4cd240dfc1d0823b0dbb5dcdd09cf27fa721d9f8
pallavi23102003 <pallavishirsath2024@gmail.com>
2025-02-06 23:01:40 +0530

    new

--- areaOfCircle.cpp
+++ areaOfCircle.cpp
@@ -14,22 +14,37 @@ using namespace std;
 
 //Calculating simple interest
 
+// int main(){
+
+//     float principle;
+//     cout<<"Enter the principle amount: ";
+//     cin>>principle;
+
+//     float rate ;
+//     cout<<"Enter the rate of interest: ";
+//     cin>>rate;
+
+//     float time;
+//     cout<<"enter the time period: ";
+//     cin>>time;
+
+//     float simpleInterest = (principle*rate*time)/100;
+//     // simpleInterest = (principle*rate*time)/100;
+//     cout<<"The calculated simple interest is:"<<simpleInterest;
+
+// }
+
+//volume of a sphere
+
 int main(){
+    
+    float r;
+    cout<<"Enter the radius of a circle: ";
+    cin>>r;
 
-    float principle;
-    cout<<"Enter the principle amount: ";
-    cin>>principle;
+    float pi = 3.14;
 
-    float rate ;
-    cout<<"Enter the rate of interest: ";
-    cin>>rate;
-
-    float time;
-    cout<<"enter the time period: ";
-    cin>>time;
-
-    float simpleInterest = (principle*rate*time)/100;
-    // simpleInterest = (principle*rate*time)/100;
-    cout<<"The simple interest is:"<<simpleInterest;
+    float volume = (4/3)*(pi*r*r*r);
+    cout<<"The volume of the circle is: "<<volume;
 
 }
