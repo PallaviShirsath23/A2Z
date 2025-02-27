dab61469cb5763c591e5cb039b12a33d20c895e6
pallavi23102003 <pallavishirsath2024@gmail.com>
2025-02-27 16:06:31 +0530

    updated

--- basics/operationsA.cpp
+++ basics/operationsA.cpp
@@ -21,6 +21,45 @@ using namespace std;
 
 //print karo sum of digits of a given number
 
+// int main(){
+//     long long int n;
+
+//     cout<<"Enter a number: ";
+//     cin>>n;
+
+//     long long int sum = 0;
+
+//     while(n!=0){
+//         int ld = n%10;
+        
+//         n=n/10;
+//         sum +=ld;
+//     }
+//     cout<<"Sum of digits: "<<sum<<endl;
+// }
+
+//proudt of digits given that the number does not contain 0
+
+// int main(){
+//     long long int n;
+//     cout<<"Enter a number: ";
+//     cin>>n;
+
+//     int product = 1;
+
+//     while(n!=0){
+//         long long int ld = n%10;
+//         n=n/10;
+//         product=product*ld;
+
+//     }
+//     cout<<"Product: "<<product;
+// }
+
+//print sum of even digits of a number
+
+
 int main(){
     
+
 }
