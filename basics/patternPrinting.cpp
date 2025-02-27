8c2e0c67679edfcc20600f1d6a4ec4c6f199fe2f
pallavi23102003 <pallavishirsath2024@gmail.com>
2025-02-27 23:12:33 +0530

    new

--- basics/patternPrinting.cpp
+++ basics/patternPrinting.cpp
@@ -54,22 +54,81 @@ using namespace std;
 //     }
 // }
 
+// int main(){
+
+//     // int a = 1;
+//     // int b = 4;
+
+//     // for(int i = 1;i<=4; i++){
+//     //     cout<<i<<" ";
+
+//     // }
+
+//     for(int i =1;i<=4;i++){
+//         for(int j =1;j<=4;j++){
+//             cout<<;
+//         }
+//     cout<<endl;
+//     }
+// }
+// int main(){
+//     for(int i =1;i<=4;i++){
+//         for(int j =1;j<=4;j++){
+//             cout<<char(j+64)<<" ";
+//         }
+//     cout<<endl;
+//     }
+// }
+
+// int main(){
+//     for(int i =1;i<=4;i++){
+//         for(int j =1;j<=4;j++){
+//             cout<<char(i+64)<<" ";
+//         }
+//     cout<<endl;
+//     }
+// }
+
+// int main(){
+//     for(int i =1;i<=4;i++){
+//         for(int j =1;j<=4;j++){
+//             cout<<char(i+64)<<" ";
+//         }
+//     cout<<endl;
+//     }
+// }
+
+//print the given pattern
+//*
+//** 
+//*** 
+// ****
+
+// int main(){
+//     int n;
+//     cout<<"Enter a number: ";
+//     cin>>n;
+
+//     for(int row =1; row<=n; row++){
+//         for(int column=1; column<=row; column++){
+//             cout<<"* ";
+//         }
+//         cout<<endl;
+
+//     }
+// }
+
 int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
 
-    // int a = 1;
-    // int b = 4;
-
-    // for(int i = 1;i<=4; i++){
-    //     cout<<i<<" ";
-
-    // }
-
-    for(int i =1;i<=4;i++){
-        for(int i =1;i<=4;i++){
-            cout<<i;
+    for(int row =1; row<=n; row++){
+        for(int column=1; column<=row; column++){
+            cout<<;
         }
-    cout<<endl;
+        cout<<endl;
+
     }
 }
-
 
