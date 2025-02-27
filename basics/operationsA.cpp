eef931d4bfd3f57a045691ec0a2a2e57bf788576
pallavi23102003 <pallavishirsath2024@gmail.com>
2025-02-27 12:50:39 +0530

    updated

---
+++ basics/operationsA.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// to count digits of a given number
+// int main(){
+
+//     long long int n;
+
+//     cout<<"Enter a number: ";
+//     cin>>n;
+
+//     // long long int count = 0;
+
+//     // while(n!=0){
+//     //     n=n/10;
+//     //     count++;
+//     // }
+//     cout<<"count: "<<int(log10(n)+1)<<endl;
+// }
+
+
+//print karo sum of digits of a given number
+
+int main(){
+    
+}
