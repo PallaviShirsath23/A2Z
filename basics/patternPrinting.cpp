d0a94c1f9e7757580da8bf5eec58e81981bf2f13
pallavi23102003 <pallavishirsath2024@gmail.com>
2025-02-27 22:08:49 +0530

    updated

---
+++ basics/patternPrinting.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+using namespace std;
+
+// int main(){
+//     cout<<"*****"<<endl;
+//     cout<<"*****"<<endl;
+//     cout<<"*****"<<endl;
+// }
+
+// int main(){
+
+//     int m;
+//     cout<<"Enter number of rows: ";
+//     cin>>m;
+
+//     for(int i =1;i<=m;i++){
+//         cout<<"*****"<<endl;
+//     }
+// }
+
+// int main(){
+
+//     int m;
+//     cout<<"Enter number of rows: ";
+//     cin>>m;
+
+//     int n;
+//     cout<<"Enter number of columns: ";
+//     cin>>n;
+
+//     for(int i =1;i<=m;i++){
+//         for(int i =1;i<=m;i++){
+//             cout<<"* ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// int main(){
+
+//     int m;
+//     cout<<"Enter number of rows: ";
+//     cin>>m;
+
+//     // int n;
+//     // cout<<"Enter number of columns: ";
+//     // cin>>n;
+
+//     for(int i =1;i<=m;i++){
+//         for(int i =1;i<=m;i++){
+//             cout<<"* ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+int main(){
+
+    // int a = 1;
+    // int b = 4;
+
+    // for(int i = 1;i<=4; i++){
+    //     cout<<i<<" ";
+
+    // }
+
+    for(int i =1;i<=4;i++){
+        for(int i =1;i<=4;i++){
+            cout<<i;
+        }
+    cout<<endl;
+    }
+}
+
+
