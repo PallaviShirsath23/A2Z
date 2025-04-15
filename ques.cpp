2e48b247b43b8b11d048467758ce0749515f29c5
Pallavi Shirsath <pallavishirsath2003@gmail.com>
2025-04-15 12:06:14 +0530

    new

--- ques.cpp
+++ ques.cpp
@@ -2,21 +2,24 @@
 using namespace std;
 
 
-int sq(int num){
-    int square = num * num;
-    // cout<<"Square of a number is: "<<square<<endl;
-    return square;
+// int sq(int num){
+//     int square = num * num;
+//     // cout<<"Square of a number is: "<<square<<endl;
+//     return square;
 
-}
+// }
 
 
 
-int main(){
-    int num, square;
-    cout<<"Enter a number: ";
-    cin>>num;
-    int val = sq(num);
-    val = sq(val);
-    cout<<"Square of "<<num<<" is: "<<val<<endl;
+// int main(){
+//     int num, square;
+//     cout<<"Enter a number: ";
+//     cin>>num;
+//     int val = sq(num);
+//     val = sq(val);
+//     cout<<"Square of "<<num<<" is: "<<val<<endl;
 
-}
+// }
+
+
+
