852cf6fa64dbfb463d015d530913cde0b8e5de92
Pallavi Shirsath <pallavishirsath2003@gmail.com>
2025-04-16 21:17:34 +0530

    updated

--- basics/synatx.cpp
+++ basics/synatx.cpp
@@ -1,13 +1,36 @@
 #include<iostream>
 using namespace std;
 
+// int main(){
+//     int arr[5];
+//     arr[0] = 23;
+//     arr[1] = 2;
+//     arr[2] = 3;
+//     arr[3] = 26;
+//     arr[4] = 28;
+
+
+// }
+// int main(){
+//     int size = 10, b[size];
+
+
+// }
+
+
+
 int main(){
-    int arr[5];
-    arr[0] = 23;
-    arr[1] = 2;
-    arr[2] = 3;
-    arr[3] = 26;
-    arr[4] = 28;
+    int n;
+    cin>>n;
+    cout<<"Input the size of array: ";
 
+    int arr[n];
 
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    for(int i = 0; i<n; i++){
+        cout<<arr[i];
+    }
 }
