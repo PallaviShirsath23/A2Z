cb78e6bfd9266e1c1576dfda2b351bf78b11bb2d
Pallavi Shirsath <pallavishirsath2003@gmail.com>
2025-04-16 18:13:23 +0530

    updated

--- basics/patterns.cpp
+++ basics/patterns.cpp
@@ -31,19 +31,38 @@ using namespace std;
 //     }
 // }
 
+// int main(){
+
+//     int n;
+//     cin>>n;
+
+//     for(int i =0; i<n; i++){
+//         for(int j= 0; j<n-i+1; j++){
+//             cout<<" ";
+//         }
+//         for( int j =0; j<2*i-1; j++){
+//             cout<<"*";
+//         }
+//         for(int j=0; j<n-i+1; j++){
+//             cout<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
 int main(){
 
     int n;
     cin>>n;
 
-    for(int i =0; i<n; i++){
-        for(int j= 0; j<n-i+1; j++){
+    for(int i =n; i<n; i++){
+        for(int j= n; j>n-i+1; j++){
             cout<<" ";
         }
-        for( int j =0; j<2*i-1; j++){
+        for( int j =n; j>2*i-1; j++){
             cout<<"*";
         }
-        for(int j=0; j<n-i+1; j++){
+        for(int j=n; j>n-i+1; j++){
             cout<<" ";
         }
         cout<<endl;
