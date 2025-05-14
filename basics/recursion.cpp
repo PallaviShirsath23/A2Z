10ac29c91bf47f962eaa0744ac62f9094f0e8dee
Pallavi Shirsath <pallavishirsath2003@gmail.com>
2025-05-14 19:57:56 +0530

    updated

---
+++ basics/recursion.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+//int cnt = 0;
+// void name(int cnt){
+//     if(cnt == 5) return;// base  case
+//     cout<<"PALLAVI"<<" ";
+//     // cnt++;
+//     name(cnt+1);
+
+
+// }
+
+// int main(){
+//     name(0);
+// }
+
+
+// void count(int i, int n){
+//     if (i>n) return;
+//     cout<<"PALLAVI ";
+//     count(i+1,n);
+
+// }
+
+// int main(){
+//     int n;
+//     cout<<"Enter the number of times you want to print your name: ";
+//     cin>>n;
+//     count(1,n);
+// }
